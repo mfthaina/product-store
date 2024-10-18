db0e54406bf54aefa31abddec400476b275bab80
Thainá Souza <thaina@tutanota.com>
2024-10-18 10:17:47 -0300

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,30 @@
 import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+const __dirname = path.resolve();
 
-app.listen(5000, () => {
-  console.log("Server is running on http://localhost:5000 ");
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
 });
